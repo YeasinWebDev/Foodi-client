61ce5c36ae40af9344b800109d082424e93ca06b
Yeasin <ar9485727@gmail.com>
2024-07-29 08:34:00 +0600

    add: Footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function Root() {
     return (
         <div className='px-4 py-5 lg:px-10 lg:py-6'>
             <Nav />
             <Outlet />
+            <Footer/>
         </div>
     )
 }
